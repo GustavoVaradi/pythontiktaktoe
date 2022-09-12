8aa10559137f2a86167a904c821314eadc6e0c7a
Gustavo Varadi <85495903+GustavoVaradi@users.noreply.github.com>
2022-09-12 18:57:00 -0300

    Initial Project for tik tak toe

---
+++ main.py
@@ -0,0 +1,9 @@
+import pygame
+
+pygame.init()
+
+image_tiktaktoe = pygame.image.load("tiktaktoe.png")
+pygame.display.init() 
+pygame.display.set_mode(size=(400,400))
+while 1:
+    pygame.display.get_active()
